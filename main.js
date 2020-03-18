66537058feec6f94b7df6dc4e72bb935aeb35ed4
arditxh1 <33842680+arditxh1@users.noreply.github.com>
2020-03-18 18:16:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ let questions = {
     1: "Do you have signs of fever?",
     2: "Do you feel fatigue?",
     3: "Do you cough?",
-    4: "A teshtini n\xEB baza normale?",
+    4: "Have you been sneezing?",
     5: "Do you feeel aches & pains?",
     6: "Do you have a runny or stuffy nose?",
     7: "Do you have a sore throat?",
@@ -114,13 +114,13 @@ function NextQuestion(button){
             resultText = "<h1 class='endTitleText'>You have symptoms between COVID-19 and the flu.</h1>"
             displayPercentages("CG", CovidAccuracy, FtofjeAccuracy, GripiAccuracy)
         }else if(diagnosis == CovidAccuracy){
-            resultText = "<h1 class='endTitleText'>There's a chance that u have COVID-19.</h1>"
+            resultText = "<h1 class='endTitleText'>There's a chance that you have COVID-19.</h1>"
             displayPercentages("C", CovidAccuracy, FtofjeAccuracy, GripiAccuracy)
         }else if(diagnosis == FtofjeAccuracy){
-            resultText = "<h1 class='endTitleText'>There's a chance that u have a cold.</h1>"
+            resultText = "<h1 class='endTitleText'>There's a chance that you have a cold.</h1>"
             displayPercentages("F", CovidAccuracy, FtofjeAccuracy, GripiAccuracy)
         }else if(diagnosis == GripiAccuracy){
-            resultText = "<h1 class='endTitleText'>There's a chance that u have the flu.</h1>"
+            resultText = "<h1 class='endTitleText'>There's a chance that you have the flu.</h1>"
             displayPercentages("G", CovidAccuracy, FtofjeAccuracy, GripiAccuracy)
         }
     }
@@ -128,11 +128,11 @@ function NextQuestion(button){
 
 function displayPercentages(type, val1, val2, val3){
     if(type == "CFG"){
-        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val2, 26) + "% that u have a cold <br> but also" + getPercentage(val3, 27) + " sure that you have the flu."
+        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val2, 26) + "% sure that you have a cold <br> but also" + getPercentage(val3, 27) + " sure that you have the flu."
     }else if(type == "CF"){
-        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val2, 26) + "% sure that u have a cold"
+        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val2, 26) + "% sure that you have a cold"
     }else if(type == "CG"){
-        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val3, 27) + "% sure that u have the flu."
+        subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19 and <br> " + getPercentage(val3, 27) + "% sure that you have the flu."
     }else if(type == "C"){
         subResultText = "We are " + getPercentage(val1, 23) + "% that you have COVID-19."
     }else if(type == "F"){
